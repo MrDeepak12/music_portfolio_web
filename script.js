3f13083e277ec065784b268e95317ebcf33e7eef
deepak_patkar <deepakpatkar2003@gmail.com>
2026-08-15 14:33:03 +0530

    modifiy second

--- script.js
+++ script.js
@@ -6,13 +6,18 @@ var musicpause = document.getElementById("pause");
 var forward = document.getElementById("forward");
 var previous = document.getElementById("previous");
 let songs = [
-    "rainbow_dash_anthem.mp3",
-    "tyr_been_dreamin.mp3"
+    "thodi-der-mp3free.org.mp3",
+    "ye-tu-ne-kya-kiya-mp3free.org.mp3",
+    "ytmp3free.cc_bahara-slowed-and-reverb-dd4lofi-lofi-music-youtubemp3free.org.mp3",
+    "ytmp3free.cc_bahara-slowed-and-reverb-dd4lofi-lofi-music-youtubemp3free.org.mp3",
+    "ytmp3free.cc_chashni-slowedreverb-lyrics-salman-katrina-youtubemp3free.org.mp3",
+    "ytmp3free.cc_tum-jo-aaye-slowed-reverb-rahat-fateh-ali-khan-tulsi-kumar-lofi-love-song-youtubemp3free.org.mp3",
 ];
 let currentsong = 0;
  play.onclick =function() {
-   
+       
         music.play();
+        
        musicpause.style.display = "flex";
        play.style.display = "none";
         soundtrack.style.animation = "rotate 10s linear infinite";
